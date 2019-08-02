3a42ff05eff4914042b2a49c4201ab2fc1f02f4f
Jason <haloos@me.com>
2019-08-02 10:17:38 -0400

    added new books

--- database.js
+++ database.js
@@ -1,5 +1,9 @@
 import { Book } from './Book.js';
+const newBook = new Book('Milkman', 287);
+const favBook = new Book('Save nothing for the next life', 345);
+const notfavBook = new Book('The Eye', 711);  
 
+var Book = ["MilkMan", "Save nothing for the next life", "The Eye"];
 
 export { bookList };
 
