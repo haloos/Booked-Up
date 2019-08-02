d12ea715a6960221288c1ad6ae4cb68dbf6412f4
Jason <haloos@me.com>
2019-08-02 09:57:15 -0400

    added readBook

---
+++ Book.js
@@ -0,0 +1,10 @@
+class Book {
+  constructor(title, author, pages, description, currentPage, read) {
+    this.title = 'Feel Free';
+    this.author = 'Zadie Smith'; 
+    this.pages = 300; 
+    this.description = 'Arranged into five sections--In the World, In the Audience, In the           Gallery, On the Bookshelf, and Feel Free--this new collection poses questions we              immediately recognize'; 
+    this.currentPage = 1; 
+    this.read = true; 
+  }
+}
