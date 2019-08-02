942f60a45800265d69aff0c23fede02cfe288c10
Jason <haloos@me.com>
2019-08-02 10:09:23 -0400

    added export Book

--- Book.js
+++ Book.js
@@ -6,5 +6,14 @@ class Book {
     this.description = 'Arranged into five sections--In the World, In the Audience, In the           Gallery, On the Bookshelf, and Feel Free--this new collection poses questions we              immediately recognize'; 
     this.currentPage = 1; 
     this.read = true; 
+  } 
+  readBook(currentPage) {
+    this.currentPage.push(currentPage); 
+    let sum = 1; 
+    for (let currentPage of this.currentPage) {
+      sum += currentPage;
+    } this.currentPage = sum = read;
   }
 }
+export { Book };
+
